436e6d78338d23e3638fd9b9e83ce51b9d68e085
Stoib <iainbatman@gmail.com>
2025-03-08 18:44:02 +0000

    Created loop for creating class table information

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template
 
 #store society data here for now, maybe store it somewhere else later, or in a file
-societyData = 
-    [
-        {'name':"board games club", 'description':"play board games etc etc etc"}
-        {'name':"computing society", 'description':"buncha nerds"}
-        {'name':"esports society", 'description':"also nerds but videogames"}
-    ]
+# societyData = [
+#         {'name':"board games club", 'description':"play board games etc etc etc"}
+#         {'name':"computing society", 'description':"buncha nerds"}
+#         {'name':"esports society", 'description':"also nerds but videogames"}]
 
 app = Flask(__name__)
 
@@ -14,6 +12,9 @@ app = Flask(__name__)
 def home():
     return render_template('base.html')
 
+@app.route('/timetable')
+def timetable():
+    return render_template('timetable.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)

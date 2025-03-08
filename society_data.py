aac2dfeb1b60b7970e37c318e6bbd944d4262dfb
Scott Hamilton <106433322+atBacon@users.noreply.github.com>
2025-03-08 19:45:56 +0000

    more societies added

--- society_data.py
+++ society_data.py
@@ -2,7 +2,11 @@
 societyData = [
         {'id':1, 'name':"boardgames", 'description':"play board games etc etc etc", 'image':"boardgames.jpg"},
         {'id':2, 'name':"computing", 'description':"buncha nerds", 'image':"ducs.png"},
-        {'id':3, 'name':"esports", 'description':"also nerds but videogames", 'image':"esports.jpg"}
+        {'id':3, 'name':"esports", 'description':"also nerds but videogames", 'image':"esports.jpg"},
+        {'id':4, 'name':"roleplay", 'description':"playing rolls, and rolling dice", 'image':"durps.webp"},
+        {'id':5, 'name':"marketing", 'description':"they do seem to be advertising right now", 'image':"marketing.webp"},
+        {'id':6, 'name':"mycology", 'description':"that means the study of mushrooms", 'image':"mycology.webp"},
+        {'id':7, 'name':"pokemon", 'description':"this one specific videogame", 'image':"pokemon.jpg"}
 ]
 
 def getSocietyById(toFind):

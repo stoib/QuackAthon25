f84437194afeba26b65a123c4a0d724f1332b0ac
Stoib <iainbatman@gmail.com>
2025-03-08 14:00:48 +0000

    managed to get the base.html file to interact with footer correctly

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('base.html')
 
-@app.route('/footer')
-def footer():
-    return render_template('footer.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)

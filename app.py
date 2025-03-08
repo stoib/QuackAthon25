baa0fd5eb70fb0ac18d66d1749db93a3e2e22332
Scott Hamilton <106433322+atBacon@users.noreply.github.com>
2025-03-08 13:20:37 +0000

    society templatebuilder added

--- app.py
+++ app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
+@app.route('/societies/<socName>')
+def user(socName):
+    societyData = 
+    [
+        {}
+    ]
+    return render_template('society.html', societyData=societyData)
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)

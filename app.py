e10aaf759ec0cd93aacf88eccd488c0f0d801fdc
Scott Hamilton <106433322+atBacon@users.noreply.github.com>
2025-03-08 14:04:53 +0000

    added basic society navigation

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template
 
+#store society data here for now, maybe store it somewhere else later, or in a file
+societyData = 
+    [
+        {'name':"board games club", 'description':"play board games etc etc etc"}
+        {'name':"computing society", 'description':"buncha nerds"}
+        {'name':"esports society", 'description':"also nerds but videogames"}
+    ]
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,12 +16,17 @@ def home():
 
 
 @app.route('/societies/<socName>')
-def user(socName):
-    societyData = 
-    [
-        {}
-    ]
-    return render_template('society.html', societyData=societyData)
+def society_detail(socName):
+    society = societyData.get(name)
+
+    if society is None:
+        return render_template('no_society.html')
+
+    return render_template('society.html', society)
+
+@app.route('/societies')
+def society_list()
+    return render_template("societies.html", societyData)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)

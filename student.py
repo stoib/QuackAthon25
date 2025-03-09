81237a99d0ad7e4fd564f621725f08549ff37257
Scott Hamilton <106433322+atBacon@users.noreply.github.com>
2025-03-09 08:48:38 +0000

    added additional testing data for timetables

--- student.py
+++ student.py
@@ -1,7 +1,7 @@
 studentData = [
-    {'id': 1, 'first_name': "Euan", 'surname': "Wilson", 'age': 20, 'modules': [1], 'societies': [2, 3, 5]},
-    {'id': 2, 'first_name': "Iain", 'surname': "Mckenzie", 'age': 19, 'modules': [1, 2], 'societies': [2, 3, 4, 6]},
-    {'id': 3, 'first_name': "Scott", 'surname': "Hamilton", 'age': 21, 'modules': [2, 1], 'societies': [1, 2, 6, 7]}
+    {'id': 1, 'first_name': "Euan", 'surname': "Wilson", 'age': 20, 'modules': [1, 4, 3], 'societies': [2, 3, 5]},
+    {'id': 2, 'first_name': "Iain", 'surname': "Mckenzie", 'age': 19, 'modules': [1, 2, 3], 'societies': [2, 3, 4, 6]},
+    {'id': 3, 'first_name': "Scott", 'surname': "Hamilton", 'age': 21, 'modules': [2, 1, 5], 'societies': [1, 2, 6, 7]}
 ]
 
 def getStudentById(toFind):

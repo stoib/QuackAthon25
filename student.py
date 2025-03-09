b77ba674c2bd79e46ed5181c447df4a75906ccbb
Stoib <iainbatman@gmail.com>
2025-03-09 10:09:11 +0000

    fixed app and student py files added map embeds for the modules

--- student.py
+++ student.py
@@ -1,7 +1,8 @@
 studentData = [
     {'id': 1, 'first_name': "Euan", 'surname': "Wilson", 'age': 20, 'modules': [1, 4, 3], 'societies': [2, 3, 5]},
     {'id': 2, 'first_name': "Iain", 'surname': "Mckenzie", 'age': 19, 'modules': [1, 2, 3], 'societies': [2, 3, 4, 6]},
-    {'id': 3, 'first_name': "Scott", 'surname': "Hamilton", 'age': 21, 'modules': [2, 1, 5], 'societies': [1, 2, 6, 7]}
+    {'id': 3, 'first_name': "Scott", 'surname': "Hamilton", 'age': 21, 'modules': [2, 1, 5], 'societies': [1, 2, 6, 7]},
+    {'id': 4, 'first_name': "Test", 'surname': "Person", 'age': 91, 'modules': [1, 2, 3, 4, 5], 'societies': [1, 2, 3, 4, 5, 6 ,7]}
 ]
 
 def getStudentById(toFind):
